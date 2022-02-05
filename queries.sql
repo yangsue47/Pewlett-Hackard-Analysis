36bf8cff67939b42662dd3e2a70f7936adf98eb0
Sue Yang <yang.sue603@gmail.com>
2022-02-05 10:02:39 -0600

    retirement info

--- queries.sql
+++ queries.sql
@@ -1,1 +1,109 @@
 -- Successful queries will be saved here
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
+-- Creating Employees table
+CREATE TABLE employees (
+	emp_no INT NOT NULL, 
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL, 
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+-- Creating Department manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Creating salaries table
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+-- Creating dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Creating titles table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+SELECT * FROM titles;
+
+-- search for employees born between 1952 & 1955
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- search for employees born in 1952
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- search for employees born in 1953
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- search for employees born in 1954
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- search for employees born in 1955
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- retirement eligibility
+SELECT first_name, last_name
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- number of employees retiring
+SELECT COUNT(first_name)
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 -- Creating Employees table
-CREATE TABLE Employees (
+CREATE TABLE employees (
 	emp_no INT NOT NULL, 
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
@@ -40,13 +40,13 @@ CREATE TABLE salaries (
 
 -- Creating dept_emp table
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Creating titles table
@@ -56,7 +56,60 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
+
+-- search for employees born between 1952 & 1955
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- search for employees born in 1952
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- search for employees born in 1953
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- search for employees born in 1954
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- search for employees born in 1955
+SELECT first_name, last_name
+FROM  employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- retirement eligibility
+SELECT first_name, last_name
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- number of employees retiring
+SELECT COUNT(first_name)
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM  employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+
+
+
+
+
+
+
